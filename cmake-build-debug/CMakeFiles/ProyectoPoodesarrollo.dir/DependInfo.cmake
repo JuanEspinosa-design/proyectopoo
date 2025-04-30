ba1ecf147a1f7bcacfbf88ec5d20de23d4b4ad4d
Juan S <juanfelipesanchez268@gmail.com>
2025-04-30 08:03:02 -0500

    probando commit

---
+++ cmake-build-debug/CMakeFiles/ProyectoPoodesarrollo.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/juanf/OneDrive/Desktop/proyectopoo/Animal.cpp" "CMakeFiles/ProyectoPoodesarrollo.dir/Animal.cpp.obj" "gcc" "CMakeFiles/ProyectoPoodesarrollo.dir/Animal.cpp.obj.d"
+  "C:/Users/juanf/OneDrive/Desktop/proyectopoo/CriaturaBase.cpp" "CMakeFiles/ProyectoPoodesarrollo.dir/CriaturaBase.cpp.obj" "gcc" "CMakeFiles/ProyectoPoodesarrollo.dir/CriaturaBase.cpp.obj.d"
+  "C:/Users/juanf/OneDrive/Desktop/proyectopoo/Planta.cpp" "CMakeFiles/ProyectoPoodesarrollo.dir/Planta.cpp.obj" "gcc" "CMakeFiles/ProyectoPoodesarrollo.dir/Planta.cpp.obj.d"
+  "C:/Users/juanf/OneDrive/Desktop/proyectopoo/main.cpp" "CMakeFiles/ProyectoPoodesarrollo.dir/main.cpp.obj" "gcc" "CMakeFiles/ProyectoPoodesarrollo.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/juanc/Desktop/CLionProjects/ProyectoPoodesarrollo
+# Install script for directory: C:/Users/juanf/OneDrive/Desktop/proyectopoo
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "C:/Users/juanc/Desktop/CLionProjects/ProyectoPoodesarrollo/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/juanf/OneDrive/Desktop/proyectopoo/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
